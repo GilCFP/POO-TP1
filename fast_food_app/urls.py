7e3a57bf2c1fa537798e26a6e26054b9f8f6b8bf
jaimeguima <jaimeguima36@gmail.com>
2025-09-24 22:07:32 -0300

    Atualizando o envio de imagens do backend

--- fast_food_app/urls.py
+++ fast_food_app/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -33,3 +35,7 @@ urlpatterns = [
     path('produtos/', include('apps.produto.urls')),
     path('pedidos/', include('apps.pedido.urls')),
 ]
+
+# Configuração para servir arquivos de mídia em ambiente de desenvolvimento
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
